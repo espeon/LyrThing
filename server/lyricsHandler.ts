8719ae97f6e3a726ac33f31fd3b1b8c58e55071a
Natalie B <22222885+espeon@users.noreply.github.com>
2024-11-22 12:05:36 -0600

    Handle case where album/artist is blank instead of null

--- server/lyricsHandler.ts
+++ server/lyricsHandler.ts
@@ -4,18 +4,17 @@ import { JLF } from "../src/types/lyrics";
 
 const BASE_URL = "https://lrclib.net/api";
 
-import { DeskThing } from "./index"
+import { DeskThing } from "./index";
 
 export default async function getCurrentLyrics(
   song: SongData
 ): Promise<JLF | string> {
-    // build query string
-    let query = new URLSearchParams();
-    query.append("track_name", song.track_name);
-    if(song.artist) query.append("artist_name", song.artist);
-    if(song.album) query.append("album_name", song.album);
-
-    
+  // build query string
+  let query = new URLSearchParams();
+  query.append("track_name", song.track_name);
+  if (song.artist && song.artist != "")
+    query.append("artist_name", song.artist);
+  if (song.album && song.album != "") query.append("album_name", song.album);
 
   let res = await fetch(BASE_URL + `/get?${query.toString()}`, {
     headers: {
@@ -25,7 +24,7 @@ export default async function getCurrentLyrics(
   });
 
   if (!res.ok) {
-    let j = await res.json()
+    let j = await res.json();
     throw new Error(res.statusText + " " + j.name + ": " + j.message);
   }
 
